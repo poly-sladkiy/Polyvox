a0933adbda18c97ed1be8a9ef8ae0aa86b82ab66
Игнаков Константин Михайлович <kmignakov@mts.ru>
2025-03-07 23:16:56 +0300

    feat: created simple audio player using NAudio

---
+++ src/ConsolePlayer/Program.cs
@@ -0,0 +1,14 @@
+﻿using NAudio.Wave;
+
+using(var audioFile = new AudioFileReader(@"C:\Users\k.ignakov\Downloads\Music\06. Первобытный страх.mp3"))
+using (var outputDevice = new WaveOutEvent())
+{
+    outputDevice.Init(audioFile);
+    outputDevice.Play();
+    while (outputDevice.PlaybackState == PlaybackState.Playing)
+    {
+        Thread.Sleep(1000);
+    }
+}
+
+Console.WriteLine("Hello, World!");
