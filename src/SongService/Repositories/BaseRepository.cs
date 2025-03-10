f9ce89514ca8e3cb20f31af9f54d77c61c4d1a47
Игнаков Константин Михайлович <kmignakov@mts.ru>
2025-03-10 12:51:27 +0300

    feat(BaseRepository): make all methods virtual

--- src/SongService/Repositories/BaseRepository.cs
+++ src/SongService/Repositories/BaseRepository.cs
@@ -17,24 +17,24 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
         _songCollection = _connectionService.GetCollection<TEntity>(collectionName);
     }
 
-    public async Task<List<TEntity>> GetAsync() =>
+    public virtual async Task<List<TEntity>> GetAsync() =>
         await _songCollection.Find(_ => true).ToListAsync();
     
-    public async Task<List<TEntity>> GetAsync(int count) =>
+    public virtual async Task<List<TEntity>> GetAsync(int count) =>
         await _songCollection.Find(_ => true).ToListAsync();
 
-    public async Task<TEntity?> GetAsync(string id) =>
+    public virtual async Task<TEntity?> GetAsync(string id) =>
         await _songCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
-    public async Task CreateAsync(TEntity newEntity) =>
+    public virtual async Task CreateAsync(TEntity newEntity) =>
         await _songCollection.InsertOneAsync(newEntity);
     
-    public async Task CreateAsync(IEnumerable<TEntity> newEntity) =>
+    public virtual async Task CreateAsync(IEnumerable<TEntity> newEntity) =>
         await _songCollection.InsertManyAsync(newEntity);
 
-    public async Task UpdateAsync(string id, TEntity updateEntity) =>
+    public virtual async Task UpdateAsync(string id, TEntity updateEntity) =>
         await _songCollection.ReplaceOneAsync(x => x.Id == id, updateEntity);
 
-    public async Task RemoveAsync(string id) =>
+    public virtual async Task RemoveAsync(string id) =>
         await _songCollection.DeleteOneAsync(x => x.Id == id);
 }
